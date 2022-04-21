195fb9f70b69d15999dfce901899d3646142a8ee
Lzsofiae <99612637+Lzsofiae@users.noreply.github.com>
2022-04-21 09:05:45 +0200

    Add files via upload

---
+++ BV9CU6_0404/openclose.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace neptunkod_openclose
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"neptunkod.txt");
+            Console.WriteLine(sr.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}
